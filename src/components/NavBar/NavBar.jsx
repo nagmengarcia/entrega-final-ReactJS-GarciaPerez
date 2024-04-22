c453cc02a8d2ee5460c85186bab3e87f20198285
nagmengarcia <nagmengarcia@gmail.com>
2024-04-22 20:26:25 -0300

    empece mi proyecto

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+const NavBar = () => {
+  return (
+    <div>
+        <div>
+            <img src="
+            " alt="" />
+        </div>
+
+        <ul>
+            <l1>Guantes</l1>
+            <l1>Tibiales</l1>
+            <l1>Vendas</l1>
+        </ul>
+        
+    </div>
+  )
+}
+
+export default NavBar
