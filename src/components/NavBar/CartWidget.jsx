16c43837e2256e5b8c6cce479663252eb11ec91f
nagmengarcia <nagmengarcia@gmail.com>
2024-04-23 18:48:56 -0300

    ultimo commit antes de entregar, sume el hook useState para emparejar al counter del cartWidget el estado inicial en 0

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { IoCartOutline } from "react-icons/io5";
 import "./CartWidget.css"
 const CartWidget = () => {
+
+    const [ counter, setCounter] = useState(0)
+
+    // const addition =() =>{
+    //     setCounter(counter+1)
+    // }
+
+
   return (
     <div className="nav-bar__cart">
         <IoCartOutline size={24} />
-        <p className="pepe">0</p>
+        <p className="counter">{counter}</p>
     </div>
   )
 }
