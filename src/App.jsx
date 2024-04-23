96f6317dc40119bd4cacc391b3feb41037dab4e3
nagmengarcia <nagmengarcia@gmail.com>
2024-04-23 01:15:16 -0300

    avance con el navBar de mi proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import './App.css'
 
 function App() {
-  
-
   return (
    <div>
-    
+    <NavBar/>
+    <ItemListContainer saludo="Bienvenido a la tienda"/>
    </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,16 @@
+import imgLogo from "../../assets/logo-tienda.png"
+import "./NavBar.css"
 const NavBar = () => {
   return (
-    <div>
+    <div className="nav-bar">
         <div>
-            <img src="
-            " alt="" />
+            <img src={imgLogo} className="img-logo" alt="" />
         </div>
 
-        <ul>
-            <l1>Guantes</l1>
-            <l1>Tibiales</l1>
-            <l1>Vendas</l1>
+        <ul className="nav-bar__link-item-group">
+            <li className="nav-bar__list-item">Guantes</li>
+            <li className="nav-bar__list-item">Tibiales</li>
+            <li className="nav-bar__list-item">Vendas</li>
         </ul>
         
     </div>
