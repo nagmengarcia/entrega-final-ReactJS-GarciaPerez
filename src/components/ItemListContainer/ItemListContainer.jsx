aa3c5554ef401bd7d10d3cd889a79377515a5f3f
nagmengarcia <nagmengarcia@gmail.com>
2024-05-27 01:46:20 -0300

    llame a los elementos correctamente desde la db en ItemListContainer, falta implementar Loader

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import getProducts from "../../data/data";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import db from "../../db/db";
 import ItemList from "./ItemList";
 import "./ItemListContainer.css";
 import { useParams } from "react-router-dom";
@@ -10,25 +11,51 @@ const ItemListContainer = () => {
   const { idCategory } = useParams();
   const { cargando, mostrarLoader, ocultarLoader, pantallaCarga } = useLoader();
 
-  //solo consumir api al principio
+  const gettingDocs = (calledDoc) => {
+    getDocs(calledDoc)
+      .then((productsFromDataBase) => {
+        const data = productsFromDataBase.docs.map((product) => {
+          return { id: product.id, ...product.data() };
+        });
+        setProducts(data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const getProducts = () => {
+    const productsRef = collection(db, "products");
+    gettingDocs(productsRef);
+  };
+
+  const getProductsByCategory = () => {
+    const productsRef = collection(db, "products");
+    const q = query(productsRef, where("category", "==", idCategory));
+    gettingDocs(q);
+  };
   useEffect(() => {
-    mostrarLoader();
-    getProducts()
-      .then((res) => {
-        if (idCategory) {
-          const productsFilter = res.filter((p) => p.category === idCategory);
-          setProducts(productsFilter);
-        } else {
-          setProducts(res);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => {
-        console.log("Promesa terminada");
-        ocultarLoader();
-      });
+    if (idCategory) {
+      getProductsByCategory();
+    } else {
+      getProducts();
+    }
+
+    // mostrarLoader();
+    // getProducts();
+    // .then((res) => {
+    //   if (idCategory) {
+    //     const productsFilter = res.filter((p) => p.category === idCategory);
+    //     setProducts(productsFilter);
+    //   } else {
+    //     setProducts(res);
+    //   }
+    // })
+    // .catch((error) => {
+    //   console.error(error);
+    // })
+    // .finally(() => {
+    //   console.log("Promesa terminada");
+    //   ocultarLoader();
+    // });
   }, [idCategory]);
 
   const capitalizeFirstLetter = (value) => {
