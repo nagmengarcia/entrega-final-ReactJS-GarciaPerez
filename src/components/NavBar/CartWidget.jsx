2776ff1037336f4412f02545ef08875f0ea75bbd
nagmengarcia <nagmengarcia@gmail.com>
2024-06-07 18:45:22 -0300

    optimize componente NavBar

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -2,7 +2,6 @@ import { IoBagRemoveOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
-import "./CartWidget.css";
 
 const CartWidget = () => {
   const { totalCartItemAmount } = useContext(CartContext);
